ec303882e10b4f64bd9a4025765f9949a5acd6e6
Jonathan Alvarez <j@jonalvarezz.com>
2024-12-06 19:46:32 -0500

    home: add more content
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,11 @@
 "use client"
 
-import { BanknotesIcon } from "@heroicons/react/24/outline"
+import {
+  BanknotesIcon,
+  ArrowsUpDownIcon,
+  BuildingStorefrontIcon,
+  LanguageIcon,
+} from "@heroicons/react/24/outline"
 
 import { Card } from "./components/card"
 import { Heading, Text } from "@chakra-ui/react"
@@ -23,6 +28,27 @@ export default function Home() {
               icon={<BanknotesIcon className="size-12 " />}
               term="React Server Actions con PostgresSQL"
               desc="Explora una aplicación de manejo de gastos que lista, crea y elimina registros desde el servidor y una DB."
+            ></Card>
+          </li>
+          <li>
+            <Card
+              icon={<ArrowsUpDownIcon className="size-12 " />}
+              term="Consumo de datos"
+              desc="Aplicación de manejor de Marcadores (Bookmarks) para explorar las diferentes estrategías y patrones para consumir datos."
+            ></Card>
+          </li>
+          <li>
+            <Card
+              icon={<LanguageIcon className="size-12 " />}
+              term="Internacionalización"
+              desc="Identifica las opciones de la nueva versión para crear sitios amigables a locales y diccionarios de traducciones."
+            ></Card>
+          </li>
+          <li>
+            <Card
+              icon={<BuildingStorefrontIcon className="size-12 " />}
+              term="Deploy"
+              desc="Aprende a correr tu aplicación de Next.js 15 en tanto en servidores autogestionados como Cloudflare como en propios."
             ></Card>
           </li>
         </ul>
