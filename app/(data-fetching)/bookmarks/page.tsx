db1b9f10bc6a85017b28c3e76e84c9c6995a226c
Jonathan Alvarez <j@jonalvarezz.com>
2024-12-08 16:39:26 -0500

    bookmark: make it client
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- app/(data-fetching)/bookmarks/page.tsx
+++ app/(data-fetching)/bookmarks/page.tsx
@@ -1,15 +1,20 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
 import { Heading, Text } from "@chakra-ui/react"
 
-import { orm } from "../db"
+import { BookmarkType } from "../schema"
 import { Bookmark } from "./components/Bookmark"
 
-export default async function Bookmarks() {
-  const bookmarks = await orm.query.bookmarks.findMany({
-    limit: 10,
-    with: {
-      author: true,
-    },
-  })
+export default function Bookmarks() {
+  const [bookmarks, setBookmarks] = useState<BookmarkType[]>([])
+
+  useEffect(() => {
+    fetch("/bookmarks/api")
+      .then((response) => response.json() as Promise<{ data: BookmarkType[] }>)
+      .then(({ data }) => setBookmarks(data))
+  }, [])
 
   return (
     <main className="container mx-auto px-4 my-16 space-y-4">
@@ -26,9 +31,9 @@ export default async function Bookmarks() {
         {bookmarks?.map((bookmark) => (
           <li className="border-b-2 py-4 px-6" key={bookmark.id}>
             <Bookmark {...bookmark} />
-            <div className="my-1 text-gray-600 text-xs ml-7">
+            {/* <div className="my-1 text-gray-600 text-xs ml-7">
               Creado por {bookmark.author?.name || "Anónimo"}
-            </div>
+            </div> */}
           </li>
         ))}
       </ul>
