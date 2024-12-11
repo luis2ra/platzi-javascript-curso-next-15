6673939b383ac3194cbc3fd1a35bea0e169f700d
Jonathan Alvarez <j@jonalvarezz.com>
2024-12-10 19:04:23 -0500

    add sentry
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
-import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs"
+import type { NextConfig } from "next"
+
+const enableSentry = Boolean(process.env.SENTRY_DSN)
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+}
 
-export default nextConfig;
+const getSentryConfig = () =>
+  withSentryConfig(nextConfig, {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    org: "personal-lsj",
+    project: "javascript-nextjs",
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    widenClientFileUpload: false,
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: false,
+  })
+
+const CONFIG = enableSentry ? getSentryConfig() : nextConfig
+
+export default CONFIG
