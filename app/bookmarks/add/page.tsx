a3d5104426fed49e9567106bf280d10e45d5fd27
Jonathan Alvarez <j@jonalvarezz.com>
2024-12-11 09:51:13 -0500

    feat: handle errors
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- app/bookmarks/add/page.tsx
+++ app/bookmarks/add/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Form from "next/form"
 
 import {
@@ -10,8 +12,11 @@ import {
 } from "@chakra-ui/react"
 
 import { addBookmark } from "./actions"
+import { useActionState } from "react"
 
-export default async function Bookmarks() {
+export default function Bookmarks() {
+  const [state, action, pending] = useActionState(addBookmark, { error: "" })
+
   return (
     <main className="mt-12">
       <header className="">
@@ -22,7 +27,7 @@ export default async function Bookmarks() {
       </header>
 
       <Form
-        action={addBookmark}
+        action={action}
         className="p-6 my-12 border-2 space-y-4 max-w-lg mx-auto"
       >
         <FormControl>
@@ -34,8 +39,12 @@ export default async function Bookmarks() {
           <Input required type="url" name="url" />
         </FormControl>
 
-        <Button type="submit">Agregar</Button>
-        {/* {error && <div className="my-4 bg-red-300 px-6 py-4">{error}</div>} */}
+        <Button disabled={pending} type="submit">
+          Agregar
+        </Button>
+        {state?.error && (
+          <div className="my-4 bg-red-300 px-6 py-4">{state.error}</div>
+        )}
       </Form>
     </main>
   )
